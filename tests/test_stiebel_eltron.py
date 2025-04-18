fb7a0a77baccbbc003e45f52e06b9c4519cb044e
Paul Frank <pail@gmx.net>
2025-04-18 12:36:57 +0000

    Add first set of tests

---
+++ tests/test_stiebel_eltron.py
@@ -0,0 +1,52 @@
+from unittest.mock import MagicMock
+import pytest
+from python_stiebel_eltron.lwz import LwzStiebelEltronAPI, LwzSystemValuesRegisters
+from python_stiebel_eltron.wpm import WpmStiebelEltronAPI, WpmSystemValuesRegisters
+from pymodbus.client import AsyncModbusTcpClient
+from pymodbus.pdu.register_message import (
+    ReadHoldingRegistersResponse,
+    ReadInputRegistersResponse,
+)
+
+
+async def read_registers(client, address: int, *, count: int = 1, slave: int = 0, no_response_expected: bool = False) -> ReadInputRegistersResponse:
+    """Read a slice from the input register."""
+    return ReadInputRegistersResponse(address=address, count=count, registers=list(range(count)))
+
+
+@pytest.mark.asyncio()
+async def test_wpm(mocker):
+    api = WpmStiebelEltronAPI("localhost")
+    mock_connect = mocker.patch("pymodbus.client.AsyncModbusTcpClient.connect")
+    mock_close = mocker.patch("pymodbus.client.AsyncModbusTcpClient.close")
+    mocker.patch("pymodbus.client.AsyncModbusTcpClient.read_holding_registers", read_registers)
+    mocker.patch("pymodbus.client.AsyncModbusTcpClient.read_input_registers", read_registers)
+
+    await api.connect()
+    mock_connect.assert_called_once()
+
+    await api.async_update()
+
+    assert api.get_register_value(WpmSystemValuesRegisters.ACTUAL_TEMPERATURE_FEK) == 0.2
+
+    await api.close()
+    mock_close.assert_called_once()
+
+
+@pytest.mark.asyncio()
+async def test_lwz(mocker):
+    api = LwzStiebelEltronAPI("localhost")
+    mock_connect = mocker.patch("pymodbus.client.AsyncModbusTcpClient.connect")
+    mock_close = mocker.patch("pymodbus.client.AsyncModbusTcpClient.close")
+    mocker.patch("pymodbus.client.AsyncModbusTcpClient.read_holding_registers", read_registers)
+    mocker.patch("pymodbus.client.AsyncModbusTcpClient.read_input_registers", read_registers)
+
+    await api.connect()
+    mock_connect.assert_called_once()
+
+    await api.async_update()
+
+    assert api.get_register_value(LwzSystemValuesRegisters.RELATIVE_HUMIDITY_HC1) == 0.2
+
+    await api.close()
+    mock_close.assert_called_once()
