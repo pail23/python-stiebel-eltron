88571abf600aee45abd2fa326fae3a84b8687e00
Manuel Stahl <thymythos@googlemail.com>
2025-07-19 16:09:21 +0200

    Fix project name and bump version

--- pystiebeleltron/__init__.py
+++ pystiebeleltron/__init__.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from pymodbus.client import AsyncModbusTcpClient
 
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
