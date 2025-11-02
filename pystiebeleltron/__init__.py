fb727292cb1ee87e472341fc926b35f1c77b7473
Manuel Stahl <thymythos@googlemail.com>
2025-11-02 14:30:53 +0100

    Bump version to 0.2.5

--- pystiebeleltron/__init__.py
+++ pystiebeleltron/__init__.py
@@ -8,7 +8,7 @@ from enum import Enum
 from pymodbus.client import AsyncModbusTcpClient
 from pymodbus.pdu import ModbusPDU
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 _LOGGER = logging.getLogger(__package__)
 
