f6ab3d4fd8dccc206d532d9a5cd93f73775e11f6
Manuel Stahl <thymythos@googlemail.com>
2026-09-18 18:32:39 +0200

    Bump version to 0.8.0

--- pystiebeleltron/__init__.py
+++ pystiebeleltron/__init__.py
@@ -7,7 +7,7 @@ from typing import Any
 from modbus_connection import ModbusError, ModbusUnit
 from modbus_connection.model import RegisterField, WriteValidator
 
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 _LOGGER = logging.getLogger(__package__)
 
